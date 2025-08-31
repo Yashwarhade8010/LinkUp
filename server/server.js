df2f02bb73048816a12702bac39b36bc890f7697
Yashwarhade8010 <96858058+Yashwarhade8010@users.noreply.github.com>
2025-08-31 17:38:04 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const {oauthoSession} = require("./config/passport")
 const userRoutes = require("./routes/userRoutes")
 const cors = require("cors");
 const { limiter } = require("./middleware/rateLimiter");
-MongoConnect();
+
 
 const app = express();
 
@@ -18,6 +18,7 @@ app.use(
 );
 app.use(express.json({ limit: "10mb" }));
 oauthoSession(app);
+MongoConnect();
 
 app.use("/", limiter, userRoutes);
 
